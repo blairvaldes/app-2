3609647f1c1728f3d308b1e155340ac2b35c661a
blairvaldes <78276792+blairvaldes@users.noreply.github.com>
2021-02-12 10:21:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
+ require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/FeatureLayer"
+      ], function (Map, MapView, FeatureLayer) {
         var map = new Map({
-          basemap: "dark-gray"
+          basemap: "hybrid"
         });
 
         var view = new MapView({
           container: "viewDiv",
           map: map,
-          zoom: 10,
-          center: [-89.85, 38.5] // longitude, latitude
+
+        
         });
-      });
+
+       
+       
+
+
+
+ var featureLayer_1 = new FeatureLayer({     url:"https://services.arcgis.com/jDGuO8tYggdCCnUJ/arcgis/rest/services/CADominantGroundwaterFeatures/FeatureServer/0"});
+   
+  
+
+        map.add(featureLayer_1);
+
+    
+  var featureLayer_2 = new FeatureLayer({     url:"https://services.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/California_Fire_Perimeters_1878_2019/FeatureServer/1"});
+   
+ 
+
+        map.add(featureLayer_2);
+});
